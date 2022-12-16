30798f56fb98afd3ca0df713c1358585f4785324
Vitor <frete497@gmail.com>
2022-12-15 22:02:23 -0400

    Benefits Folder

---
+++ src/components/Benefits/Benefits.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Benefits = () => {
+  return (
+    <Container>Benefits</Container>
+  )
+}
+
+const Container = styled.div``
+
+export default Benefits
