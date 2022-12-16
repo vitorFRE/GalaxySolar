f03650d48dfa93fc76396ea5b747d0b19b710315
Vitor <frete497@gmail.com>
2022-12-16 16:47:34 -0400

    OurClients Folder

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,19 @@
-import Banner from "../components/Banner/Banner"
-import Benefits from "../components/Benefits/Benefits"
-import Header from "../components/Header/Header"
-import NumbersInfos from "../components/NumberInfos/NumbersInfos"
+import Banner from '../components/Banner/Banner';
+import Benefits from '../components/Benefits/Benefits';
+import Header from '../components/Header/Header';
+import NumbersInfos from '../components/NumberInfos/NumbersInfos';
+import Clients from '../components/OurClients/Clients';
 
 function Home() {
   return (
     <>
-     <Header />
-     <Banner />
-     <NumbersInfos />
-     <Benefits />
+      <Header />
+      <Banner />
+      <NumbersInfos />
+      <Benefits />
+      <Clients />
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
